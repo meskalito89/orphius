06f0fc0e8ceb58a595f359a81b1ba6ebd77a0a39
borisjhonson <rokinea89@gmail.com>
2024-05-09 18:10:51 +0500

    Добавил классу PitchLession функцию to_wav для сохранения файла миди в wav формат

--- PitchLession.py
+++ PitchLession.py
@@ -1,11 +1,14 @@
 from midiutil import MIDIFile
+from midi2audio import FluidSynth
 from random import randint
+import tempfile
+from pdb import set_trace
 
 class PitchLession(MIDIFile):
     def __init__(self,
         note: int,
         distance: int,
-        output_file: str,
+        # output_file: str,
         track = 0,
         channel = 0,
         time = 0, 
@@ -26,7 +29,21 @@ class PitchLession(MIDIFile):
 
         self.addNote(self.track, self.channel, self.note, time*4, self.duration, self.volume)
         self.addNote(self.track, self.channel, self.note + self.distance, time*4+1, self.duration, self.volume)
-
-        with open(output_file, 'wb') as file:
-            self.writeFile(file)
+        # with open(output_file, 'wb') as file:
+        #     self.writeFile(file)
     
+    # def open(self):
+    #     self.file = tempfile.NamedTemporaryFile('+wb')
+    #     self.writeFile(self.file)
+    #     self.file.seek(0)
+    #     return self.file
+        
+    def to_wav(self, sound_font: str, output_file: str):
+        fs = FluidSynth(sound_font)
+        with tempfile.NamedTemporaryFile('b+w') as midi_file:
+            self.writeFile(midi_file)
+            midi_file.seek(0)
+            fs.midi_to_audio(midi_file.file.name, output_file)
+
+
+
--- main.py
+++ main.py
@@ -1,26 +1,60 @@
 from PitchLession import PitchLession
 from midi2audio import FluidSynth
+import tempfile
 import wave
+from pdb import set_trace
 
 REPEAT = 2
 SOUND_FONT = 'sound_fonts/Yamaha_C3_Grand_Piano.sf2'
 
-def create_lession():
 
-    PitchLession(note=60, distance=7, output_file='output.mid')
-    fs = FluidSynth(SOUND_FONT)
-    fs.midi_to_audio("output.mid", 'output.wav')
-    data = []
-
-    for i in range(REPEAT):
-        with wave.open('output.wav') as w:
-            data.append( [w.getparams(), w.readframes(w.getnframes())] )
-
-    with wave.open('result.wav', 'wb') as result:
-        result.setparams(data[0][0])
-        for i in range(len(data)):
-            result.writeframes(data[i][1])
-
-# print(create_lession())
+# def connect_wav_files(wav_files:list[bytearray]) -> bytearray :
+#     HEAD_BYTES_IN_WAV = 0
+#     tmp_file = tempfile.TemporaryFile('+ba')
+#     for i, file in enumerate(wav_files):
+#         file.seek(HEAD_BYTES_IN_WAV)
+#         if i == 0:
+#            file.seek(0) 
+#         tmp_file.write(file.read())
+#     tmp_file.seek(0)
+#     return tmp_file
 
 
+
+
+# def create_lession():
+#     PitchLession(note=60, distance=7, output_file='output.mid')
+#     fs = FluidSynth(SOUND_FONT)
+#     fs.midi_to_audio("output.mid", 'output.wav')
+#     data = []
+#     for i in range(REPEAT):
+#         with wave.open('output.wav') as w:
+#             data.append( [w.getparams(), w.readframes(w.getnframes())] )
+#     with wave.open('answers/2.wav') as w:
+#         data.append([w.getparams(), w.readframes(w.getnframes())])
+#     with wave.open('result.wav', 'wb') as result:
+#         result.setparams(data[0][0])
+#         for i in range(len(data)):
+#             result.writeframes(data[i][1])
+
+# def create_lession():
+#     l = PitchLession(note=60, distance=-7)
+#     with tempfile.NamedTemporaryFile('b+w') as midi_file, tempfile.NamedTemporaryFile('b+w') as wav_file:
+#         l.writeFile(midi_file)
+#         midi_file.seek(0)
+#         fs = FluidSynth(SOUND_FONT)
+#         fs.midi_to_audio(midi_file.name, wav_file.name)
+#         with open('output.wav', 'bw') as f:
+#             result = connect_wav_files([wav_file, wav_file, wav_file])
+#             f.write(result.read())
+
+# create_lession()
+
+def main():
+    l = PitchLession(note=60, distance=7)
+    with tempfile.NamedTemporaryFile('b+w') as wav_file:
+        l.to_wav('sound_fonts/Yamaha_C3_Grand_Piano.sf2', wav_file.name)
+    
+
+if __name__ == "__main__":
+    main()
