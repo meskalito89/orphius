142f78099af19bba73a8b1642b63edd8a12a736e
borisjhonson <rokinea89@gmail.com>
2024-05-12 15:18:39 +0500

    Закончил. Буду думать об оптимизации

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PitchLession import PitchLession
 import pydub
 import tempfile
 import random
+import os
 
 REPEAT = 2
 SOUND_FONT = 'sound_fonts/Yamaha_C3_Grand_Piano.sf2'
@@ -30,11 +31,11 @@ ANSWERS = [
 def main():
     files = []
     for i in range(COUNT_OF_LESSIONS):
+        random_note = random.randomint(60, 80)
         distance1 = random.randint(0, 12)
-        print(distance1)
         distance_is_greather_then_0 = random.choice([-1, 1])
         distance2 = distance1 * distance_is_greather_then_0
-        l = PitchLession(sound_font='sound_fonts/Yamaha_C3_Grand_Piano.sf2', note=60, distance=distance2, repeat=2)
+        l = PitchLession(sound_font='sound_fonts/Yamaha_C3_Grand_Piano.sf2', note=random_note, distance=distance2, repeat=2)
         pitch_file = l.to_temporary_file()
         pitch = pydub.AudioSegment.from_wav(pitch_file.name)
         files.append(pitch)
@@ -45,8 +46,16 @@ def main():
     for wav in files[1:]:
         combined = combined.append(wav)
 
-    with tempfile.NamedTemporaryFile('w+') as result:
-        combined.export(result.name, format='wav')
+    # with tempfile.NamedTemporaryFile('w+') as result:
+    with open('meta.txt', 'r+') as file:
+        key, last_file_name = file.readline().split("=")
+        file.seek(0)
+        os.remove(f'{last_file_name}.mp3')
+        file_name, number = last_file_name.split('_')
+        new_file_name = '_'.join([file_name, str(int(number) + 1)])
+        file.write(f'{key}={new_file_name}')
+        with open(f'{new_file_name}.mp3', 'wb') as result:
+            combined.export(result.name, format='mp3')
 
 if __name__ == "__main__":
     main()
